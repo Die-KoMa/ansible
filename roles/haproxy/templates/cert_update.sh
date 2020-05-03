11c1ef32f6d98d39a4960c0dbd8f59789b6ee077
Jan Beinke <git@janbeinke.com>
2020-05-03 19:33:32 +0200

    Fix haproxy not having its certs properly

--- roles/haproxy/templates/cert_update.sh
+++ roles/haproxy/templates/cert_update.sh
@@ -3,7 +3,9 @@
 {{ ansible_managed|comment }}
 
 for path in /etc/letsencrypt/live/*; do
-  cat ${path}/{fullchain,privkey}.pem > "/etc/haproxy/certs/$(basename "$path").pem"
+  if [[ -f ${path}/fullchain.pem ]]; then
+    cat ${path}/{fullchain,privkey}.pem > "/etc/haproxy/certs/$(basename "$path").pem"
+  fi
 done
 
 systemctl is-active --quiet haproxy.service && systemctl reload haproxy.service
